1235df543ea659be610c2bf16f106be3f0cdaa15
Talakr34 <talakronz@gmail.com>
2026-04-27 16:57:57 +0200

    Añadir información

--- src/src/Hola.java
+++ src/src/Hola.java
@@ -1,5 +1,7 @@
 public class Hola {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
+        System.out.println("Linea nueva 1");
+        System.out.println("Linea nueva 2");
     }
 }
